10596d38616249a16fe51a274f0d8bfcae04c3de
jianxun <jianxun@guanghe.tv>
2018-11-24 14:41:10 +0800

    [0][fix] fix puppeteer not launch in ubuntu

--- app/helper/browser.helper.js
+++ app/helper/browser.helper.js
@@ -18,7 +18,8 @@ class BrowserHelper {
       // 打开开发者工具, 当此值为true时, headless总为false
       devtools: false,
       // 关闭headless模式, 会打开浏览器
-      headless: true
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
     })
   }
   getJPGPath () {
